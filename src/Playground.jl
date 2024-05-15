a99881f16bb47462f992e1fd0b8afd7a81ade5c0
Rithik Appachi Senthilkumar <r.appachisenthilkumar@student.tudelft.nl>
2024-05-15 16:51:09 +0200

    mnist model and initial impl of autopgd

--- src/Playground.jl
+++ src/Playground.jl
@@ -1,47 +1,144 @@
 # println("Hello World!")
-using BSON
-using BSON: @save, @load
+# import Pkg
+# Pkg.add(whatever)
+
+using Serialization: serialize, deserialize
 using Random
 using Flux, Statistics, ProgressMeter, Plots, TaijaData, Distances
 using Flux.Data: DataLoader
-using Flux: onehotbatch, onecold, crossentropy, throttle, update!, push!
+using Flux: onehotbatch, onecold, crossentropy, logitcrossentropy, mse, throttle, update!, push!
 using Base.Iterators: repeated, partition
 
 x_train, y_train = TaijaData.load_mnist()
 x_test, y_test = TaijaData.load_mnist_test()
 
-x_train = reshape(x_train, (28, 28, 1, 60000))
-x_test = reshape(x_test, (28, 28, 1, 10000))
+m = deserialize("play_models/trained_mnist.jls")
+loss(x, y) = logitcrossentropy(m(x), y)
 
-x_first_10k = x_train[:, :, :, 1:10000]
-y_first_10k = y_train[1:10000]
+# SIMPLE ADVERSARIAL ATTACKS (obtained from https://github.com/jaypmorgan/Adversarial.jl/tree/master)
 
-m = BSON.load("play_models/cnn_10k_5_32.bson")[:m2] 
-θ = Flux.params(m)
-loss(x, y) = crossentropy(m(x), y)
-acc(x, y) = mean(m(x) .== Flux.onecold(y))
-opt = ADAM()
-
-function FGSM(model, loss, x, y; ϵ = 0.5, clamp_range = (-1, 1))
+function FGSM(model, loss, x, y; ϵ = 1, clamp_range = (-1, 1))
     J = gradient(() -> loss(x, y), Flux.params([x]))
     x_adv = clamp.(x .+ (Float32(ϵ) * sign.(J[x])), clamp_range...)
     return x_adv
 end
 
-function PGD(model, loss, x, y; ϵ = 10, step_size = 0.01, iters = 100, clamp_range = (-1, 1))
+function PGD(model, loss, x, y; ϵ = 1, step_size = 0.001, iterations = 100, clamp_range = (-1, 1))
     x_adv = clamp.(x + (randn(Float32, size(x)...) * Float32(step_size)), clamp_range...); # start from the random point
     δ = Distances.chebyshev(x, x_adv)
-    iter = 1; while (δ < ϵ) && iter <= iters
+    iteration = 1; while (δ < ϵ) && iteration <= iterations
         x_adv = FGSM(model, loss, x_adv, y; ϵ = step_size, clamp_range = clamp_range)
         δ = chebyshev(x, x_adv)
-        iter += 1
+        iteration += 1
     end
     return x_adv
 end
 
+# ATTACKS FOR AUTO-ATTACK
+# AutoPGD: For now, only the linf norm
+function AutoPGD(model, x, y, iterations, ϵ, loss, ρ = 0.75, α = 0.75, clamp_range = (-1, 1))
+    step_size = 2ϵ
+
+    # Initializing period lengths and checkpoints
+    period_lengths = [0.0, 0.22]
+    checkpoints = [0, ceil(Int, period_lengths[2] * iterations)]
+
+    # Computing period lengths and checkpoints
+    for j = 3:iterations
+        period_length = max(period_lengths[j-1] - period_lengths[j-2] - 0.03, 0.06)
+        push!(period_lengths, period_length)
+        push!(checkpoints, ceil(Int, (period_lengths[j] + period_lengths[j-1]) * iterations))
+    end
+
+    points = []
+    losses = []
+    max_losses = []
+    step_sizes = []
+    push!(step_sizes, step_size)
+
+    push!(points, x)
+    initial_gradient = gradient(() -> loss(x, y), Flux.params([x]))
+    push!(losses, loss(x, y))
+
+    x_1 = clamp.(x .+ step_size * sign.(initial_gradient[x]), clamp_range...)
+    push!(points, x_1)
+    f_1 = loss(x_1, y)
+    push!(losses, f_1)
+    push!(max_losses, f_1)
+
+    x_max = 0
+
+    f_max = max(losses[1], losses[2])
+
+    if f_max == losses[1]
+        x_max = x
+    else
+        x_max = x_1
+    end
+
+    for k = 2:iterations
+        last_point = last(points)
+        last_hit_checkpoint = 0
+        last_hit_checkpoint_index = 1
+        current_gradient = gradient(() -> loss(last_point, y), Flux.params([last_point]))
+
+        z_k_p_1 = clamp.(last_point .+ step_size * sign.(current_gradient[last_point]), clamp_range...)
+        x_k_p_1 = clamp.(last_point + α * (z_k_p_1 - last_point) + (1 - α) * (last_point - points[k-1]), clamp_range...)
+
+        new_point_loss = loss(x_k_p_1, y)
+
+        if new_point_loss > f_max
+            x_max = x_k_p_1
+            f_max = new_point_loss
+        end
+
+        if k in checkpoints
+            condition_1 = false
+            count = 0
+
+            for i = last_hit_checkpoint_index:k-1
+                if loss(points[i+1], y) > loss(points[i], y)
+                    count += 1
+                end
+            end
+
+            if count < ρ * (k - last_hit_checkpoint)
+                condition_1 = true
+            end
+
+            condition_2 = false
+
+            if step_sizes[last_hit_checkpoint_index] == step_size && max_losses[last_hit_checkpoint_index] == f_max
+                condition_2 = true
+            end
+
+            if condition_1 || condition_2
+                step_size /= 2
+                x_k_p_1 = x_max
+            end
+        end
+        last_hit_checkpoint = k
+        last_hit_checkpoint_index = findlast(item -> item == k, checkpoints)
+        push!(step_sizes, step_size)
+        push!(max_losses, f_max)
+        push!(points, x_k_p_1)
+    end
+
+    return x_max
+end
+
+function FAB(model, x, y, restarts, iterations, α_max, β, η, ϵ, p)
+
+end
+
+function SquareAttack()
+
+end
+
 function plot_image(adv_example, clean_example, clean_pred_label, predicted_label, true_label)
     println("Digit predicted by the model for the clean image: ", clean_pred_label)
-    println("Digit predicted by the model for the adversarial image: ", predicted_label)
+    println("Digit predicted by the model for the 
+    adversarial image: ", predicted_label)
     println("True label: ", true_label)
 
     image_matrix = reshape(adv_example, 28, 28)
@@ -50,10 +147,9 @@ function plot_image(adv_example, clean_example, clean_pred_label, predicted_labe
     clean_img_matrix = reshape(clean_example, 28, 28)
     clean_img_matrix = reverse(permutedims(clean_img_matrix, [2, 1]), dims=1)
     hms = []
-    push!(hms, heatmap(image_matrix, color=:grays, axis=false, title="$true_label -> $predicted_label"))
-    push!(hms, heatmap(clean_img_matrix, color=:grays, axis=false, title="$true_label -> $clean_pred_label"))
+    push!(hms, heatmap(image_matrix, color=:grays, axis=false, title="Adversarial example: $true_label -> $predicted_label"))
+    push!(hms, heatmap(clean_img_matrix, color=:grays, axis=false, title="Clean example: $true_label -> $clean_pred_label"))
     plot(hms..., layout=(1, 2), colorbar=false)
-
 end
 
 
@@ -67,20 +163,27 @@ real_example = 0
 true_label = -1
 index = -1
 
+
+
 while adv_pred == real_pred
-    index = rand(1:60000)
-    real_example = reshape(x_train[:, :, :, index], 28, 28, 1, 1)
+    index = rand(1:10000)
+    real_example = x_train[:, index]
     true_label = y_train[index]
-    adversarial_example = FGSM(m, loss, real_example, true_label)
+    # adversarial_example = FGSM(m, loss, real_example, true_label)
+    adversarial_example = AutoPGD(m, real_example, true_label, 50, 8/255, loss)
     # adversarial_example = PGD(m, loss, real_example, true_label)
     adv_pred = m(adversarial_example) |> Flux.onecold |> getindex
     real_pred = m(real_example) |> Flux.onecold |> getindex
     attempts_taken += 1
 end
 
+
+
 println("attempts taken to find adversarial example that classifies different: ", attempts_taken)
 println("index identified: ", index)
 plot_image(adversarial_example, real_example, real_pred - 1, adv_pred - 1, true_label)
+
+
 
 
 
