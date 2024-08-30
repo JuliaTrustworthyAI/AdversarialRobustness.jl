767d86bbfc3d6aac2b707d8a7671aecb4dbbd58d
Rithik Appachi Senthilkumar <r.appachisenthilkumar@student.tudelft.nl>
2024-08-29 20:28:21 -0400

    adversarial training. FGSM-training works as expected

--- src/training/adversarial_training.jl
+++ src/training/adversarial_training.jl
@@ -32,7 +32,7 @@ function vanilla_train(model, x_train, y_train, max_epochs, batch_size; loss=log
     return vanilla_losses
 end
 
-function adversarial_train(model, x_train, y_train, epochs, batch_size, ϵ; loss=logitcrossentropy, opt=Adam, step_size = 0.01, iterations = 10, attack_method = :FGSM, min_label=0, max_label=9, clamp_range=(0, 1))
+function adversarial_train(model, x_train, y_train, epochs, batch_size, ϵ; loss=logitcrossentropy, opt=ADAM, step_size = 0.03, iterations = 10, attack_method = :FGSM, min_label=0, max_label=9, clamp_range=(0, 1))
     adv_losses = []
     θ = Flux.params(model)
     train_loader = DataLoader((x_train , y_train), batchsize=batch_size, shuffle=true)
@@ -42,28 +42,31 @@ function adversarial_train(model, x_train, y_train, epochs, batch_size, ϵ; loss
         epoch_loss = 0.0
 
         for (idx, (x, y)) in enumerate(train_loader)
-            y_onehot = onehotbatch(y, label_min:label_max)
+            println("batch ", idx)
+            y_onehot = onehotbatch(y, min_label:max_label)
             x_adv = zeros(size(x))
 
             if attack_method == :FGSM
-                x_adv = FGSM(model, x, y; loss=cross_entropy_loss, ϵ = ϵ, min_label=min_label, max_label=max_label, clamp_range = (0, 1))
+                for i = 1:size(x)[4]
+                    x_adv[:, :, :, i] = FGSM(model, x[:, :, :, i], y[i]; ϵ = ϵ)
+                end
             elseif attack_method == :PGD
-                x_adv = PGD(model, x, y; loss=cross_entropy_loss, ϵ = ϵ, min_label=0, max_label=9, clamp_range = (0, 1))
+                for i = 1:size(x)[4]
+                    x_adv[:, :, :, i] = PGD(model, x[:, :, :, i], y[i]; ϵ = ϵ, step_size=step_size, iterations=iterations)
+                end
             else 
                 error("Unsupported attack method: $attack_method")
             end
             
             l_adv = 0.0;
-            l_nat = 0.0;
 
             grads = Flux.gradient(θ) do 
                 l_adv = loss(model(x_adv), y_onehot)
-                l_nat = loss(model(x), y_onehot)
-                return l_adv + l_nat
+                return l_adv
             end
 
             update!(opt, θ, grads)
-            epoch_loss += (l_adv + l_nat)
+            epoch_loss += (l_adv)
 
         end
 
--- temp/src/Playground.jl
+++ temp/src/Playground.jl
@@ -1,4 +1,5 @@
 using Flux, TaijaData, Random, Distances, AdversarialRobustness, BSON
+using BSON: @save
 include("utils/plot.jl")
 
 # CNN() = Chain(
@@ -11,42 +12,51 @@ include("utils/plot.jl")
 #     x -> reshape(x, :, size(x, 4)),
 #     Dense(288, 10)) |> gpu
 
-# model = CNN();
-# loss(x, y) = logitcrossentropy(x, y)
+# model_adv_test = CNN();
+# loss(x, y) = Flux.logitcrossentropy(x, y)
 # opt = ADAM()
 
 # vanilla_losses = vanilla_train(model, loss, opt, X, y, 5, 32, 0, 9)
 
-X, y = load_mnist()
+# Adv train a dummy model based on a random architecture (jaypmorgan to be precise)
+# model_adv_test = CNN()
+# adv_losses = adversarial_train(model_adv_test, X, y, 20, 32, 0.3; loss = loss, attack_method = :FGSM, opt=opt)
+# @save "temp/src/models/MNIST/convnet_jaypmorgan_advt_test.bson" model_adv_test
+
+X, y = load_mnist(10000)
 X = (X .+ 1) ./ 2
-X = reshape(X, 28, 28, 1, 60000)
+X = reshape(X, 28, 28, 1, 10000)
 
-model = BSON.load("temp/src/models/MNIST/convnet_jaypmorgan.bson")[:model]
+# model = BSON.load("temp/src/models/MNIST/convnet_jaypmorgan.bson")[:model]
+model_adv = BSON.load("temp/src/models/MNIST/convnet_jaypmorgan_advt_test.bson")[:model_adv_test]
 
-idx = rand(1:1000)
+model_to_use = model
+# model_to_use = model_adv
+
+idx = rand(1:10000)
 
 X_try = X[:, :, :, idx]
 y_try = y[idx]
 
-target = 1
+target = -1
 
-# x_best_fgsm = FGSM(model, X_try, y_try; ϵ = 0.2)
-# x_best_pgd = PGD(model, X_try, y_try; ϵ = 0.3, step_size=0.02, iterations=40)
-# x_best_square, n_queries = SquareAttack(model, X_try, y_try, 5000; ϵ = 0.3, verbose=true)
-x_best_autopgd, η_list, checkpoints, starts_updated = AutoPGD(model, X_try, y_try, 100; ϵ = 0.2, target=target)
+x_best_fgsm = FGSM(model_to_use, X_try, y_try; ϵ = 0.3)
+# x_best_pgd = PGD(model_to_use, X_try, y_try; ϵ = 0.3, step_size=0.02, iterations=20)
+# x_best_square, n_queries = SquareAttack(model_to_use, X_try, y_try, 5000; ϵ = 0.3, verbose=true)
+# x_best_autopgd, η_list, checkpoints, starts_updated = AutoPGD(model_to_use, X_try, y_try, 100; ϵ = 0.2, target=target)
 
-println(extrema(x_best_autopgd .- X_try))
-
-# attack_to_use = x_best_fgsm
-# attack_to_use = x_best_pgd
+attack_to_use = x_best_fgsm
+# attack_to_use = x_best_pgda
 # attack_to_use = x_best_square
-attack_to_use = x_best_autopgd
+# attack_to_use = x_best_autopgd
+
+println(extrema(attack_to_use .- X_try))
 
 clean_img = X_try[:, :, :, 1:1]
 adv_img = attack_to_use[:, :, :, 1:1]
 
-clean_pred = model(clean_img)
-adv_pred = model(adv_img)
+clean_pred = model_to_use(clean_img)
+adv_pred = model_to_use(adv_img)
 
 clean_pred_label = (clean_pred |> Flux.onecold |> getindex) - 1
 adv_pred_label = (adv_pred |> Flux.onecold |> getindex) - 1
