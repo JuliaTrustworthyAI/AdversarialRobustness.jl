87d8df9af2a2701f8d2c17a94bcdee6d6fd7030a
pat-alt <altmeyerpat@gmail.com>
2024-09-05 16:55:24 +0200

    sorted

--- test/aqua.jl
+++ test/aqua.jl
@@ -4,5 +4,5 @@ using Aqua
     # Ambiguities needs to be tested seperately until the bug in Aqua package (https://github.com/JuliaTesting/Aqua.jl/issues/77) is fixed
     Aqua.test_ambiguities([AdversarialRobustness]; recursive=false, broken=false)
 
-    Aqua.test_all(AdversarialRobustness; ambiguities=false, piracies=false)
+    Aqua.test_all(AdversarialRobustness; ambiguities=false)
 end
